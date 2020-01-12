a5f609e6936e050f1b75c080e31156050f9b800c
Cheeze <cheeze.cake@kakaocommerce.com>
2020-01-12 15:57:23 +0900

    init project

---
+++ src/main/java/com/nexters/mnt/MntApplication.java
@@ -0,0 +1,13 @@
+package com.nexters.mnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MntApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nexters/mnt/MntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexters.mnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MntApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
