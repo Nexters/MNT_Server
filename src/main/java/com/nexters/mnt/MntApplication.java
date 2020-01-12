6cef0b4bcb721f9176ffe880f276ccf719ff2938
wisdompark96 <youo78@naver.com>
2020-01-12 16:39:52 +0900

    test

--- src/main/java/com/nexters/mnt/MntApplication.java
+++ src/main/java/com/nexters/mnt/MntApplication.java
@@ -1,13 +1,18 @@
 package com.nexters.mnt;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class MntApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MntApplication.class, args);
+        System.out.println();
     }
 
 }
