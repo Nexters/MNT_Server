08f11bddee7cf7b7cb915dec66fa73cc657edabb
Cheeze <cheeze.cake@kakaocommerce.com>
2020-03-24 21:57:22 +0900

    될려나

--- src/main/java/com/nexters/mnt/controller/MissionController.java
+++ src/main/java/com/nexters/mnt/controller/MissionController.java
@@ -54,7 +54,7 @@ public class MissionController {
             @RequestParam String userId,
             @RequestParam Long missionId,
             @RequestParam(required = false) String content,
-            @RequestParam(required = false) MultipartFile img) {
+            @RequestBody(required = false) MultipartFile img) {
         UserMissionRequest userMission = new UserMissionRequest(roomId, userId, missionId, content,new Date(), img);
         return missionService.sendMission(userMission);
     }
